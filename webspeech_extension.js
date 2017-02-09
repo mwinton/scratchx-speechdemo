24f86c398911a0424f21caa143e51c17828ba600
Mike Winton <winton@winton-macbookair.roam.corp.google.com>
2017-02-09 09:50:56 -0800

    change reg name

--- webspeech_extension.js
+++ webspeech_extension.js
@@ -41,5 +41,5 @@ new (function() {
     };
 
     // Register the extension
-    ScratchExtensions.register('Random wait extension', descriptor, ext);
+    ScratchExtensions.register(‘Web speech extension', descriptor, ext);
 })();
