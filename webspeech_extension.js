516fcb77a4d15bbb0bd9e8c651ef3d8d84c5c3f0
mwinton <winton@google.com>
2017-02-09 16:58:34 -0800

    commented out voice

--- webspeech_extension.js
+++ webspeech_extension.js
@@ -44,7 +44,7 @@ new (function() {
             
             var msg = new SpeechSynthesisUtterance(message);
             var voices = window.speechSynthesis.getVoices();
-            msg.voice = voices[10]; // Note: some voices don't support altering params
+//            msg.voice = voices[10]; // Note: some voices don't support altering params
             msg.voiceURI = 'native';
             msg.volume = 1; // 0 to 1
             msg.rate = 1; // 0.1 to 10
