577371039ea61c78113b627e484115a6eeb47ff2
mwinton <winton@google.com>
2017-02-15 11:12:41 -0800

    Update webspeech_extension.js

--- webspeech_extension.js
+++ webspeech_extension.js
@@ -58,7 +58,7 @@ new (function() {
           jsonp: "json_callback",
           success: function(data) {
             console.log('Success getting translation data');
-            translated_text = [data.translations[0].translatedText];
+            translated_text = [data.translations[0].translated_text];
             console.log('Translation is '+translated_text);
             callback(translated_text);
           },
